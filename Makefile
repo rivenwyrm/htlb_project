08b3594bb3a1c28fb2a64b9aa0e9bf32fdef1e29
rivenwyrm <rivenwyrm@gmail.com>
2016-01-26 15:52:58 -0800

    Improve makefile and test-app functionality, add -g for grabmem use

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-clean:
-	rm -f ./test_app
-test_app:
+test_app: htlb_app.c
 	gcc -Wall htlb_app.c -o test_app
 all: test_app
+
+clean:
+	rm -f ./test_app
--- htlb_app.c
+++ htlb_app.c
@@ -2,23 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
 
 #define BUFSIZE 12
 #define FILE_PATH_NR_HP "/proc/sys/vm/nr_hugepages"
 
-int parse_set(int argc, char *argv[])
+int parse_arg(int argc, char *argv[], int argind, char *str)
 {
     int args = -1;
 
-    if (argc < 3) {
+    if (argc < argind + 1) {
         return args;
     }
 
-    if (strcmp(argv[1], "-s") != 0) {
+    if (strcmp(argv[argind], str) != 0) {
         return args;
     }
 
-    args = atoi(argv[2]);
+    args = atoi(argv[argind + 1]);
 
     return args;
 }
@@ -48,26 +50,61 @@ int print_cur_hp(void)
 int set_hp(int set_val)
 {
     char buf[BUFSIZE * 5];
-    int ret = -1;
+    int ret;
 
-    if (set_val == -1) {
-        return ret;
+    switch (fork()) {
+    case 0:
+        if (set_val == -1) {
+            return -1;
+        }
+        snprintf(buf, BUFSIZE*5, "echo %d > %s", set_val, FILE_PATH_NR_HP);
+        ret = system(buf);
+        printf("set: %d / %d\n", ret, set_val);
+
+        exit(0);
+    case EAGAIN:
+    case ENOMEM:
+        return -1;
+    default:
+        return 0;
+    }
+}
+
+int grab_mem(int grab)
+{
+    if (grab == -1) {
+        return -1;
     }
 
-    snprintf(buf, BUFSIZE*5, "echo %d > %s", set_val, FILE_PATH_NR_HP);
-    ret = system(buf);
-    printf("set: %d / %d\n", ret, set_val);
-
-    return ret;
+    if (mmap(0, 2 << grab, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0) == MAP_FAILED) {
+        return -1;
+    } else {
+        return 0;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int set_val;
+    int set_val, argind = 1, grab_val;
 
-    set_val = parse_set(argc, argv);
+    set_val = parse_arg(argc, argv, argind, "-s");
+    if (set_val != -1) {
+        argind += 2;
+    }
+    grab_val = parse_arg(argc, argv, argind, "-g");
 
     print_cur_hp();
+
+    if (grab_val != -1) {
+        mlockall(MCL_FUTURE);
+        /* Could release but exit also releases */
+        if (grab_mem(grab_val) == -1) {
+            printf("failed to mmap\n");
+        } else {
+            printf("mmap'd %d\n", 2 << grab_val);
+            sleep(10);
+        }
+    }
 
     if (set_val != -1) {
         set_hp(set_val);
