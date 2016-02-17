84c97d9863d89258371515ebafc8f4e3dc985598
rivenwyrm <rivenwyrm@gmail.com>
2016-02-17 15:57:33 -0800

    Nearly finished

--- parse_results.py
+++ parse_results.py
@@ -1,10 +1,17 @@
-#!/bin/python
+#!/usr/bin/python
 
 import io
 import os
 
 def parse_file(dir, filep):
     tmpf = open(dir + filep)
+    filep = filep[3:]
+    filep = filep.replace(".", " ")
+    if filep.find(" time") != -1:
+        filep = filep[0:filep.find(" time")]
+    else:
+        filep = filep[0:filep.find(" out")]
+
     printstr=filep+","
     for line in tmpf:
         if "curhp" in line:
